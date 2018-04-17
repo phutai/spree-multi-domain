3e20109a0c87a1dbde4feff02dfcfcb2087702d9
Tai Nguyen <npt789@gmail.com>
2018-04-17 13:34:06 +0900

    hot fix update current_store is missing

--- lib/spree_multi_domain/multi_domain_helpers.rb
+++ lib/spree_multi_domain/multi_domain_helpers.rb
@@ -8,6 +8,10 @@ module SpreeMultiDomain
       receiver.send :helper_method, :current_store
       receiver.send :helper_method, :current_tracker
     end
+    
+    def current_store
+      @current_store ||= Spree::Store.current(request.env['SERVER_NAME'])
+    end
 
     def current_tracker
       @current_tracker ||= Spree::Tracker.current(request.env['SERVER_NAME'])
